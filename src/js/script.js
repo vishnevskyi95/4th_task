9c0224a6c4ddf0fb9fb8b25c5a6510db1a44b698
Gleb <vishnyagleb2@gmail.com>
2022-05-20 23:42:24 +0300

    carousel+1st_step_tabs

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-    $('.carousel__iner').slick({
+    $('.carousel__inner').slick({
+        speed: 700,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    focusOnSelect: true
+                }
+            }
 
+        ]
     });
 });
